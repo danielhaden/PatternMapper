c78bbb0e7a4b9c093b95602e1e67c56c79881f9c
Daniel Haden <DANIEL.HADEN@UCDENVER.EDU>
2022-12-12 21:13:55 -0700

    added folder expanded view

--- PatternMapper/PatternMapperToolWindowControl.xaml.cs
+++ PatternMapper/PatternMapperToolWindowControl.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Threading;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -15,6 +23,113 @@ namespace PatternMapper
         public PatternMapperToolWindowControl()
         {
             this.InitializeComponent();
+            Loaded += OnLoadedEventHandler;
+        }
+
+        private void OnLoadedEventHandler(object sender, RoutedEventArgs e)
+        {
+            FolderView.Items.Clear();
+
+            foreach ( var proj in getProjectsInSolution( getCurrentSolution() ))
+            {
+                var item = new TreeViewItem();
+
+                item.Header = proj.Name;
+                item.Tag = proj.UniqueName;
+
+                item.Items.Add(null);
+
+                item.Expanded += Folder_Expanded;
+
+                FolderView.Items.Add(item);
+            }
+        }
+
+        private static Project getProjectByName(IVsSolution solution, string uniqueName)
+        {
+            IVsHierarchy ppHierarchy;
+            solution.GetProjectOfUniqueName(uniqueName, out ppHierarchy);
+
+            return asProject(ppHierarchy);
+        }
+
+        private static List<Project> getProjectsInSolution(IVsSolution solution)
+        {
+            var projects = new List<Project>();
+            var nullGuid = Guid.Empty;
+            solution.GetProjectEnum((uint)__VSGETPROJFILESFLAGS.GPFF_SKIPUNLOADEDPROJECTS, ref nullGuid, out IEnumHierarchies enumPtr);
+
+            if (enumPtr != null)
+            {
+                uint actualResult = 0;
+                IVsHierarchy[] nodes = new IVsHierarchy[1];
+
+                while (0 == enumPtr.Next(1, nodes, out actualResult))
+                {
+                    Project project = asProject(nodes[0]);
+                    if (project != null)
+                    {
+                        projects.Add(project);
+                    }
+                }
+            }
+            return projects;
+        }
+
+        private static Project asProject(IVsHierarchy hierarchy)
+        {
+            Object obj;
+            hierarchy.GetProperty((uint)Microsoft.VisualStudio.VSConstants.VSITEMID_ROOT, (int)Microsoft.VisualStudio.Shell.Interop.__VSHPROPID.VSHPROPID_ExtObject, out obj);
+
+            return (Project) obj;
+        }
+
+        private static IVsSolution getCurrentSolution()
+        {
+            IVsSolution solution = (IVsSolution)Package.GetGlobalService(typeof(IVsSolution));
+            solution.GetSolutionInfo(out string solutionDirectory, out string solutionName, out string solutionDirectory2);
+
+            return solution;
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem) sender;
+
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            item.Items.Clear();
+
+            //var proj = (Project)item.Tag;
+
+            item.Items.Add(Directory.GetFiles(item.Tag.ToString()));
+
+            //try
+            //{
+            //    var dirs = Directory.GetDirectories(fullPath);
+
+            //    if (dirs.Length > 0)
+            //        directories.AddRange(dirs);
+            
+            //}
+            //catch { }
+
+            //directories.ForEach(directoryPath =>
+            //{
+            //    var subItem = new TreeViewItem()
+            //    {
+            //        Header = Path.GetDirectoryName(directoryPath),
+            //        Tag = directoryPath
+            //    };
+
+            //    subItem.Items.Add(null);
+
+            //    subItem.Expanded += Folder_Expanded;
+
+            //    item.Items.Add(subItem);
+            //});
+            
         }
 
         /// <summary>
